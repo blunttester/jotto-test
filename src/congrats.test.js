3a17e97d0a7616fe04a0df019e18f7abb47c9b37
Jan Matilainen <jama22@handelsbanken.se>
2021-04-23 10:17:40 +0200

    add proptypes for congrats component

--- src/congrats.test.js
+++ src/congrats.test.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import { findByTestAttr } from '../test/testUtils';
+
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false };
 /**
  * @function setup
  * @param {object} props 
  * @returns {ShallowWrapper}
  */
 const setup = (props={}) => {
-    return shallow(<Congrats {...props} />)
-}
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Congrats {...setupProps} />);
+};
+
 describe('Congrats tests', () => {
     test('renders without error', () => {
-        const wrapper = setup();
+        const wrapper = setup({ success: false });
         const component = findByTestAttr(wrapper, 'component-congrats');
         expect(component).toHaveLength(1);
      });    
@@ -31,12 +35,24 @@ describe('Congrats tests', () => {
 
     test('renders no text when success prop is null', () => {
 
-        const wrapper = setup({ success: true});
+        const wrapper = setup({ success: null});
         const message = findByTestAttr(wrapper, 'congrats-message');
-        expect(message.text()).not.toHaveLength(0);
+        expect(message).toHaveLength(0);
      });
 
-    test('renders no text when success prop is undefined', () => { });
+    test('renders no text when success prop is undefined', () => { 
+        const wrapper = setup({ success: undefined});
+        const message = findByTestAttr(wrapper, 'congrats-message');
+        expect(message).toHaveLength(0);
+    });
 
-    test('renders non-empty text when success prop is true', () => { });
+    test('renders non-empty text when success prop is true', () => { 
+        const wrapper = setup({ success: true });
+        const message = findByTestAttr(wrapper, 'congrats-message');
+        expect(message.text()).not.toHaveLength(0);});
+    
+    test('does not throw warning with expected props', () => {
+        const expectedProps = { success: false };
+        checkProps(Congrats, expectedProps);
+    });
 })
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper 
@@ -5,5 +7,16 @@
  * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
-wrapper.find('[data-test="${val}"]');
+return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        // eslint-disable-next-line react/forbid-foreign-prop-types
+        component.propTypes,
+        conformingProps,
+        'prop',
+        component.name
+        );
+        expect(propError).toBeUndefined();
 }
