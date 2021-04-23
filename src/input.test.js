80b7403c246f71beefee1643882e4d857d5c9fb6
Jan Matilainen <jama22@handelsbanken.se>
2021-04-23 15:36:04 +0200

    input proptypes tests

--- src/input.test.js
+++ src/input.test.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import checkPropTypes from 'check-prop-types';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './input';
 
+const defaultProps = {
+    secretWord: ''
+}
 /**
  * setup function for Input component
  * @returns {shallowWrapper}
  */
-const setup = () => {
-    return shallow(<Input/>);
+const setup = (props) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Input {...setupProps} />);
 };
 
 test('renders without error', () => {
     const wrapper = setup();
     const inputComponent = findByTestAttr(wrapper, 'component-input');
     expect(inputComponent).toHaveLength(1);
+});
+
+test('does not throw warning with expected props', () => {
+    const expectedProps = { secretWord: 'false' };
+    checkProps(Input, expectedProps);
+});
+
+test('throws warning with unexpected props', () => {
+    const unexpectedProps = { secretWord: false };
+    const propError = checkPropTypes(
+    // eslint-disable-next-line react/forbid-foreign-prop-types
+    Input.propTypes,
+    unexpectedProps,
+    'prop',
+    Input.name
+    );
+    expect(propError).toBeTruthy();
+
 });
