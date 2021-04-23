541bb20ccdc7848a88099181e27a062587919572
Jan Matilainen <jama22@handelsbanken.se>
2021-04-23 16:16:10 +0200

    state controlled input field

--- src/input.test.js
+++ src/input.test.js
@@ -40,3 +40,18 @@ test('throws warning with unexpected props', () => {
     expect(propError).toBeTruthy();
 
 });
+
+describe('state controlled input field', () => {
+    test('state updates with value of input box upon change', () =>{
+        const mockSetCurrentGuess = jest.fn();
+        React.useState = jest.fn(() => ['', mockSetCurrentGuess]); 
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'train' }};
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+     })
+})
