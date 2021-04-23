4ad19bd86da7f5bbd4ff5eae9cccf8c74ab737c7
Jan Matilainen <jama22@handelsbanken.se>
2021-04-23 13:41:00 +0200

    TEsts and code for non zero guessed words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -42,7 +42,31 @@ describe('if there are no words guessed', () => {
 });
 
 describe('if there are words guessed', () => {
-
+    let wrapper;
+    const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3,},
+        {guessedWord: 'agile', letterMatchCount: 1,},
+        {guessedWord: 'party', letterMatchCount: 5,}
+    ];
+    beforeEach(() => {
+        wrapper = setup({ guessedWords });
+    })
+    test('renders without error', () =>{
+        const component = findByTestAttr(wrapper, 'component-guessed-words')
+        expect(component).toHaveLength(1);
+    });
+    test('renders guessed words section', () =>{
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+        expect(guessedWordsNode).toHaveLength(1);
+    });
+    test('correct number of guessed words', () =>{
+        const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word')
+        expect(guessedWordsNodes).toHaveLength(guessedWords.length);
+    });
+    test('does not render instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions')
+        expect(instructions).toHaveLength(0);
+    });
 });
 
 
